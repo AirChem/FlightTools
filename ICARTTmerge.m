74592d3b4d74235d89807479d9d2d631d7fa4836
Glenn Wolfe <glenn.m.wolfe@nasa.gov>
2019-09-26 11:15:32 -0400

    Update ICARTTmerge.m

--- ICARTTmerge.m
+++ ICARTTmerge.m
@@ -1,16 +1,15 @@
 function D = ICARTTmerge(fldr,timeInfo,alignInfo)
 % function D = ICARTTmerge(fldr,timeInfo,alignInfo)
 % tool to merge ICARTT files.
-% requirements for this to work:
-% 1) all files to be merged are in a single directory containing no other files
-% 2) all data must be 1 Hz
-% 3) at least 3 planets must be aligned.
+% All files to be merged are in a single directory containing no other files.
 % 
 % INPUTS:
 % fldr: full path to directory containing files to merge
-% timeInfo: two-element cell array containing info for the master time vector.
+% timeInfo: two or three-element cell array containing info for the master time vector.
 %   First cell is the data ID for the file containing the master time vector.
-%   Second cell is the name of the time variable.
+%   Second cell is the name of the time variable (or start time if using a window merge).
+%   Third cell (optional) is name of stop time if using a window merge.
+%
 % alignInfo: OPTIONAL cell array indicating which variables to align by lag-covariance.
 %   First cell is master alignment variable name.
 %   Subsequent cells are names of variables to align.
@@ -24,6 +23,7 @@ function D = ICARTTmerge(fldr,timeInfo,alignInfo)
 % D = ICARTTmerge(fldr,timeInfo,alignInfo);
 %
 % 20190803 GMW
+% 20190821 GMW  Added ability to do a window merge (e.g. on WAS data).
 
 if nargin<3, alignInfo = {}; end
     
@@ -47,17 +47,28 @@ timeDataID = strrep(timeInfo{1},'-','_');
 t1name = timeInfo{2};
 assert(isfield(d,timeDataID),'timeInfo dataID %s not found in folder %s.',timeDataID,fldr)
 assert(isfield(d.(timeDataID),t1name),'timeInfo variable name %s not found in dataID %s.',t1name,timeDataID)
-D = d.(timeDataID);
+D = d.(timeDataID); %initialize output structure
 t1 = D.(t1name);
 
-% adjust for time offsets
+% get stop time for window if indicated
+if length(timeInfo)==3
+    t1name_stop = timeInfo{3};
+    assert(isfield(d.(timeDataID),t1name_stop),'timeInfo variable name %s not found in dataID %s.',t1name_stop,timeDataID)
+    t1stop = D.(t1name_stop);
+else
+    t1stop=[];
+end
+
+% adjust for time offsets (assuming 1 Hz)
 % BinAvg routine assumes midpoint
-switch(lower(t1name))
-    case 'time_start'
-        t1 = t1+0.5;
-    case 'time_mid'
-    case 'time_stop'
-        t1 = t1-0.5;
+if isempty(t1stop)
+    switch(lower(t1name))
+        case 'time_start'
+            t1 = t1+0.5;
+        case 'time_mid'
+        case 'time_stop'
+            t1 = t1-0.5;
+    end
 end
 
 % average other variables
@@ -69,14 +80,16 @@ for i = 1:length(dnames)
     t2name = ddnames{1};
     t2 = dd.(t2name);
     
-    % adjust for time offsets
+    % adjust for time offsets (assuming 1 Hz)
     % assumes 1s data
-    switch(lower(t2name))
-        case 'time_start'
-            t2 = t2+0.5;
-        case 'time_mid'
-        case 'time_stop'
-            t2 = t2-0.5;
+    if isempty(t1stop)
+        switch(lower(t2name))
+            case 'time_start'
+                t2 = t2+0.5;
+            case 'time_mid'
+            case 'time_stop'
+                t2 = t2-0.5;
+        end
     end
     
     % average
@@ -86,7 +99,12 @@ for i = 1:length(dnames)
         while isfield(D,n)
             n = [n '_'];
         end
-        D.(n) = BinAvg(t2,dd.(ddnames{j}),t1);
+        
+        if isempty(t1stop)
+            D.(n) = BinAvg(t2,dd.(ddnames{j}),t1);
+        else
+            D.(n) = BinAvg(t2,dd.(ddnames{j}),[t1 t1stop]);
+        end
     end
     
 end
